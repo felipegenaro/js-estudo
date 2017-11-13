ea38289beb836afb13c7a9466917479e2e0f20bc
Felipe Corniani de Genaro <felipe.genaro@localhost.localdomain>
2017-11-13 13:01:09 -0200

    porquice_v2

--- estudo.js
+++ estudo.js
@@ -3,11 +3,11 @@ var z = Array.apply(null, Array(v+1)).map(function (x, i) { return i; }); // giv
 var w = Array.apply(null, Array(v+1)).forEach(alert); // gives you 5 alerts
 
 
-var v = 5;
+var v = 5000; // mais de 5K o pensamento fica maior de 500ms
 var x = Array(v); // gives you an array with lentgth 5 but no values
 for (var i = v - 1; i >= 0; i--) {
 	var r = Math.floor(Math.random() * v + 1);
-	while ($.inArray(r,x) && $.inArray(r,x) > -1) {r = Math.floor(Math.random() * v + 1);}
+	while ($.inArray(r,x) > -1) r = Math.floor(Math.random() * v + 1);
 	x[i] = r;
 }
 
