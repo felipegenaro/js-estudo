d18c9ec012d36380d0eadf88ccf00738d1c6b3db
Felipe Corniani de Genaro <felipe.genaro@localhost.localdomain>
2017-11-13 12:43:32 -0200

    porquice

--- estudo.js
+++ estudo.js
@@ -7,7 +7,7 @@ var v = 5;
 var x = Array(v); // gives you an array with lentgth 5 but no values
 for (var i = v - 1; i >= 0; i--) {
 	var r = Math.floor(Math.random() * v + 1);
-	while ($.inArray(r,x)) {r = Math.floor(Math.random() * v + 1);}
+	while ($.inArray(r,x) && $.inArray(r,x) > -1) {r = Math.floor(Math.random() * v + 1);}
 	x[i] = r;
 }
 
