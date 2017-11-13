04cd144722fafa4d9f07acfe81cb989ac0fcea5a
Felipe Corniani de Genaro <felipe.genaro@localhost.localdomain>
2017-11-13 12:32:50 -0200

    bullshit

---
+++ estudo.js
@@ -0,0 +1,14 @@
+var y = Array.apply(null, Array(v+1)).map(function () {}); // gives you an array with length 5 and undefined as values 
+var z = Array.apply(null, Array(v+1)).map(function (x, i) { return i; }); // gives you an array with length 5 and values 0,1,2,3,4
+var w = Array.apply(null, Array(v+1)).forEach(alert); // gives you 5 alerts
+
+
+var v = 5;
+var x = Array(v); // gives you an array with lentgth 5 but no values
+for (var i = v - 1; i >= 0; i--) {
+	var r = Math.floor(Math.random() * v + 1);
+	while ($.inArray(r,x)) {r = Math.floor(Math.random() * v + 1);}
+	x[i] = r;
+}
+
+console.log(x);
